2bf6e7cc2ba166667c6b148b491cc184fed98c07
saurabhsingh-cedcoss <74813543+saurabhsingh-cedcoss@users.noreply.github.com>
2020-12-10 19:10:24 +0530

    Add files via upload

--- slide.js
+++ slide.js
@@ -1,55 +1,71 @@
-var a = $('.block').length;
+var a = $('.block1').length;
 
-console.log(a);
 var c = 1;
+console.log(a);
+console.log(c);
 $(document).ready(function () {
     setInterval(() => {
 
-        $('.block1').animate({
-            marginLeft: '-=100vw'
-        }, 3000, function () {
-            c++;
-            if(c==1)
-            {
-                $('.r1').prop('checked',true);
+        c++;
+        if(c==1)
+        {
+            $('.r1').prop('checked',true);
+            $('.r3').prop('checked',false);
+            $('.r2').prop('checked',false);
+
+            
+        }
+        else if(c==2)
+        {   
+            $('.r1').prop('checked',false);
+            $('.r3').prop('checked',false);
+            $('.r2').prop('checked',true);
+        }
+        else if (c == 3) {
+            $('.r1').prop('checked',false);
+            $('.r2').prop('checked',false);
+            $('.r3').prop('checked',true);
+            $('.block1').css('marginLeft', '0');
+            c = 1;
+        }
+        if(c<4){
+
+            $('.block1').animate({ marginLeft: '-=100%' });
+        }
+          else if(c==4){
+                $('.block1').css('marginLeft','0');
+                c=1;
             }
-            else if(c==2)
-            {
-                $('.r2').prop('checked',true);
-            }
-            else if (c == a) {
-                $('.block1').css('marginLeft', '0');
-                c = 1;
-            }
-        })
-    }, 5000);
+        }, 2000);
+
+        
+   
    
 
-
-    $("#next").on('click', function () {
-        $('.block1').animate({
-            marginLeft: '-100vw'
-        }, 3000, function () {
+    $(".next").on('click', function () {
+        $('.block1').css(
+        "marginLeft",'-=100%')
             c++;
-            console.log(c, a);
-            if (c == a) {
+            console.log(c);
+
+            if (c == 3) {
                 $('.block1').css('marginLeft', '0');
-                c = 1;
-            }
-        })
+                c = 0;
+            
+        };
     });
-     $("#prev").on('click', function () {
-        $('.block1').animate({
-            marginLeft: '0px'
-         },3000,function () {
-             c++;
-             console.log(c,a);
-             if(c==a)
+
+     $(".prev").on('click', function () {
+         c--;
+        $('.block1').css(
+            'marginLeft', '+=100%'
+         )
+             if(c<=1)
              {
-                 $('.block1').css('marginRight','200vw');
-             c=1;
+                 $('.block1').css('marginLeft','-100%');
+             c=3;
              }
-           })
+           
      });   
 
  $('.r1').on('click',function(){
